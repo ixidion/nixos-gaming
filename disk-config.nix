3260f6ec70da06cd68ce5898de11382ed78dbfb9
ixidion <ixidion.ninja@gmail.com>
2024-07-19 08:56:40 +0200

    New to nix. Intitial stuff for disko.

---
+++ disk-config.nix
@@ -0,0 +1,76 @@
+{
+  disko.devices = {
+    disk = {
+      vdb = {
+        type = "disk";
+        device = "/dev/vda";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              size = "2G";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [
+                  "defaults"
+                ];
+              };
+            };
+            luks = {
+              size = "100%";
+              content = {
+                type = "luks";
+                name = "crypted";
+                extraOpenArgs = [ ];
+		askPassword = true;
+                settings = {
+                  # if you want to use the key for interactive login be sure there is no trailing newline
+                  # for example use `echo -n "password" > /tmp/secret.key`
+                  #keyFile = "/tmp/secret.key";
+                  allowDiscards = true;
+                };
+                #additionalKeyFiles = [ "/tmp/additionalSecret.key" ];
+                content = {
+                  type = "lvm_pv";
+                  vg = "pool";
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+    lvm_vg = {
+      pool = {
+        type = "lvm_vg";
+        lvs = {
+          root = {
+            size = "100%FREE";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/";
+              mountOptions = [
+                "defaults"
+              ];
+            };
+          };
+#          home = {
+#            size = "10M";
+#            content = {
+#              type = "filesystem";
+#              format = "ext4";
+#              mountpoint = "/home";
+#            };
+#          };
+#          raw = {
+#            size = "10M";
+#          };
+        };
+      };
+    };
+  };
+}
